2e3c24810d0e80d3a95ac95103aacd44242728f1
Aaryansh <90237993+Aar-v@users.noreply.github.com>
2023-09-08 18:03:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const router = Router();
+const router = express.Router();
 const port = process.env.PORT || 3000;
 var fs = require('fs');
 app.use(bodyParser.json());
